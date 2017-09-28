93ad3a871761b570ae13013662659ae03c518bef
xxvms <tom@pcservicegroup.co.uk>
2017-09-28 11:34:36 +0100

    final testing in existing project

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "training on command line github" << '\n';
+
+    std::cout << "next update" << '\n';
     return 0;
 }
